4fb96c2fdfd19bdb58f28bd4b7ceb6f56e857c29
Kiko <kristiqnenchevv@gmail.com>
2023-03-11 13:13:22 +0200

    Add swagger to dependency scope

--- server/server.js
+++ server/server.js
@@ -5,12 +5,28 @@ import { config } from './Config/variables.js';
 import { expressConfig } from './Config/express.js';
 import { filter } from './Config/filter.js';
 import { router } from './Routes/router.js';
+import { specs } from './Utils/swagger.js';
+import swaggerUi from 'swagger-ui-express';
 
 const app = express();
 
 const start = async () => {
   connectDb(config.dbConnection);
   expressConfig(app, express);
+
+  if (process.env.NODE_ENV !== 'production') {
+    app.use('/swagger', swaggerUi.serve);
+    app.get(
+      '/swagger',
+      swaggerUi.setup(specs, {
+        explorer: true,
+        customCss: '.swagger-ui .topbar { display: none }',
+        customSiteTitle: 'SHOP4E API Documentation',
+      })
+    );
+    console.log('Swagger documentation available at /swagger');
+  }
+
   router(app);
   filter(app);
 
