d5f9575c4c1cfff38651b67daded15230bac6ac1
Kiko <kristiqnenchevv@gmail.com>
2023-03-13 13:25:42 +0200

    Add router documentation

--- server/Routes/router.js
+++ server/Routes/router.js
@@ -5,6 +5,49 @@ import userUploadRouter from './uploadRoutes.js';
 import userRouter from './userRoutes.js';
 
 export const router = (app) => {
+  /**
+   * @swagger
+   * components:
+   *   securitySchemes:
+   *     bearerAuth:
+   *       type: http
+   *       scheme: bearer
+   *       bearerFormat: JWT
+   *   responses:
+   *     UnauthorizedError:
+   *       description: Access token is missing or invalid
+   *     ForbiddenError:
+   *       description: Not authorized to access this resource
+   *     ValidationError:
+   *       description: Invalid input data
+   *     NotFoundError:
+   *       description: Resource not found
+   *     ServerError:
+   *       description: Internal server error
+   */
+
+  /**
+   * @swagger
+   * tags:
+   *   - name: Users
+   *     description: User management endpoints
+   *   - name: Authentication
+   *     description: Authentication endpoints
+   *   - name: Products
+   *     description: Product management endpoints
+   *   - name: Upload
+   *     description: File upload endpoints
+   *   - name: Seeder
+   *     description: Database seeding endpoints (Development only)
+   */
+
+  /**
+   * @swagger
+   * servers:
+   *   - url: /api
+   *     description: API Base URL
+   */
+
   app.use('/api/auth', authRouter);
   app.use('/api/users', userRouter);
   app.use('/api/seed', seedRouter);
