820c5a6beb006b604774340facd0360da0e046cf
Kiko <kristiqnenchevv@gmail.com>
2023-03-15 14:11:01 +0200

    Add upload routes documentation

--- server/Routes/uploadRoutes.js
+++ server/Routes/uploadRoutes.js
@@ -5,6 +5,60 @@ import * as controller from '../Controllers/uploadController.js';
 const userUploadRouter = express.Router();
 const upload = multer();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Upload
+ *   description: File upload endpoints
+ */
+
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     UploadResponse:
+ *       type: object
+ *       properties:
+ *         url:
+ *           type: string
+ *           format: uri
+ *           description: URL of the uploaded file
+ *         public_id:
+ *           type: string
+ *           description: Public ID of the uploaded file in Cloudinary
+ */
+
+/**
+ * @swagger
+ * /api/upload/profile:
+ *   post:
+ *     summary: Upload profile picture
+ *     tags: [Upload]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         multipart/form-data:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               file:
+ *                 type: string
+ *                 format: binary
+ *                 description: Profile image file
+ *     responses:
+ *       200:
+ *         description: Profile image uploaded successfully
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/UploadResponse'
+ *       400:
+ *         description: Invalid file type or size
+ *       401:
+ *         description: Not authenticated
+ */
 userUploadRouter.post(
   '/profile',
   upload.single('file'),
