aa443ba8ef12e2ccf462d6abb8c57458ba442c81
Kiko <kristiqnenchevv@gmail.com>
2023-03-30 14:41:45 +0300

    Update Upload controller

--- server/Controllers/uploadController.js
+++ server/Controllers/uploadController.js
@@ -1,26 +1,19 @@
-import { v2 as cloudinary } from 'cloudinary';
-import streamifier from 'streamifier';
-const { CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET } =
-  process.env;
+import { imageService } from '../services/ImageService.js';
 
 export const uploadPicture = async (req, res) => {
-  cloudinary.config({
-    cloud_name: CLOUDINARY_CLOUD_NAME,
-    api_key: CLOUDINARY_API_KEY,
-    api_secret: CLOUDINARY_API_SECRET,
-  });
-  const streamUpload = (req) => {
-    return new Promise((resolve, reject) => {
-      const stream = cloudinary.uploader.upload_stream((error, result) => {
-        if (result) {
-          resolve(result);
-        } else {
-          reject(error);
-        }
-      });
-      streamifier.createReadStream(req.file.buffer).pipe(stream);
-    });
-  };
-  const result = await streamUpload(req);
-  res.send(result);
+  try {
+    const url = await imageService.uploadImage(req.file, 'avatars');
+    res.send({ url });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to upload image' });
+  }
+};
+
+export const uploadMultiplePictures = async (req, res) => {
+  try {
+    const urls = await imageService.uploadMultipleImages(req.files, 'avatars');
+    res.send({ urls });
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to upload images' });
+  }
 };
