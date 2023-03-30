fbc8e002c9e2a569e83b125ec46427fde1413423
Kiko <kristiqnenchevv@gmail.com>
2023-03-30 12:40:23 +0300

    Update express configurations

--- server/Config/express.js
+++ server/Config/express.js
@@ -1,22 +1,64 @@
 import cors from 'cors';
+import { paths } from './variables.js';
+
 const whitelist = ['http://localhost:3000', 'http://localhost:5000'];
 
 export const expressConfig = (app, express) => {
-  app.use(express.static('public'));
-  app.use(cors({ origin: whitelist, creadentias: true }));
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: true }));
+  app.use(express.static(paths.publicDir));
+
+  app.use(
+    '/uploads',
+    express.static(paths.uploadsDir, {
+      maxAge: process.env.NODE_ENV === 'production' ? '1d' : 0,
+      etag: true,
+      lastModified: true,
+      index: false,
+      setHeaders: (res) => {
+        res.set('X-Content-Type-Options', 'nosniff');
+        if (process.env.NODE_ENV === 'production') {
+          res.set('Cache-Control', 'public, max-age=86400');
+        }
+      },
+    })
+  );
+
+  app.use(
+    cors({
+      origin: (origin, callback) => {
+        if (!origin || whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
+      credentials: true,
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    })
+  );
+
+  app.use(express.json({ limit: '10mb' }));
+  app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
   app.use((error, req, res, next) => {
     try {
-      if (res.headerSent) {
+      if (res.headersSent) {
         return next(error);
       }
-      res
-        .status(error.code || 500)
-        .json({ message: error.message || 'An unknown error occurred!' });
-    } catch (error) {
-      errorHandler(error, res, req);
+
+      if (error.type === 'entity.too.large') {
+        return res.status(413).json({
+          message: 'File size too large',
+          code: 413,
+        });
+      }
+
+      res.status(error.code || 500).json({
+        message: error.message || 'An unknown error occurred!',
+        code: error.code || 500,
+      });
+    } catch (err) {
+      errorHandler(err, res, req);
     }
   });
 };
