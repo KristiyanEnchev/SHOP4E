cebbef379c2c83b5b7eb741ce38029831667f8d6
Kiko <kristiqnenchevv@gmail.com>
2023-03-30 11:17:01 +0300

    Update filer and varibles

--- server/Config/filter.js
+++ server/Config/filter.js
@@ -1,19 +1,39 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import { errorHandler } from '../Utils/errorHandler.js';
+import { paths } from './variables.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const allowed = ['.js', '.css', '.png', '.jpg', '.jpeg', '.ico'];
+const allowed = [
+  '.js',
+  '.css',
+  '.png',
+  '.jpg',
+  '.jpeg',
+  '.ico',
+  '.webp',
+  '.gif',
+  '.svg',
+];
 
 export const filter = (app) => {
   app.get('*', (req, res) => {
     try {
-      if (allowed.filter((ext) => req.url.indexOf(ext) > 0).length > 0) {
-        res.sendFile(path.resolve(`public/${req.url}`));
+      if (req.url.startsWith('/uploads/')) {
+        const filePath = path.join(
+          paths.uploadsDir,
+          req.url.replace('/uploads/', '')
+        );
+        res.sendFile(filePath);
+        return;
+      }
+
+      if (allowed.some((ext) => req.url.toLowerCase().endsWith(ext))) {
+        res.sendFile(path.join(paths.publicDir, req.url));
       } else {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        res.sendFile(path.join(paths.publicDir, 'index.html'));
       }
     } catch (error) {
       errorHandler(error, res, req);
--- server/Config/variables.js
+++ server/Config/variables.js
@@ -1,7 +1,41 @@
-const { PORT, HOST, MONGO_DB_NAME, MONGO_URI } = process.env;
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export const paths = {
+  baseDir:
+    process.env.NODE_ENV === 'production' ? '/usr/src/app' : process.cwd(),
+  get uploadsDir() {
+    return path.join(this.baseDir, 'public', 'uploads');
+  },
+  get publicDir() {
+    return path.join(this.baseDir, 'public');
+  },
+};
+
+export const getMongoUri = () => {
+  const isLocal = process.env.NODE_ENV === 'development' && !process.env.DOCKER;
+  const host = isLocal ? 'localhost' : 'mongodb';
+  return `mongodb://${host}:27017/${process.env.MONGO_DB_NAME}`;
+};
 
 export const config = {
-  port: PORT || 3005,
-  host: HOST,
-  dbConnection: `${MONGO_URI}/${MONGO_DB_NAME}`,
+  port: process.env.PORT || 3005,
+  host: process.env.HOST,
+  dbConnection: getMongoUri(),
+};
+
+export const storageConfig = {
+  type: process.env.STORAGE_TYPE || 'local',
+  cloudinary: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    apiSecret: process.env.CLOUDINARY_API_SECRET,
+  },
+  local: {
+    basePath: paths.uploadsDir,
+    baseUrl: '/uploads',
+  },
 };
