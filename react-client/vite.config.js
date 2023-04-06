ae10bfbd5faaaf5ced0e0f4d011da0f2700c032f
Kiko <kristiqnenchevv@gmail.com>
2023-04-06 13:58:45 +0300

    Upgrade solution to VITE

---
+++ react-client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      // eslint-disable-next-line no-undef
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
