5ce00296694434dbb80c419c9a4ce16bd8da28c1
Kiko <kristiqnenchevv@gmail.com>
2023-05-17 12:38:36 +0300

    Update logger and authmiddleware

--- server/Config/logger.js
+++ server/Config/logger.js
@@ -1,6 +1,5 @@
 import winston from 'winston';
 import path from 'path';
-
 const { combine, timestamp, printf, colorize, errors } = winston.format;
 
 const logFormat = printf(({ level, message, timestamp, stack }) => {
@@ -8,37 +7,48 @@ const logFormat = printf(({ level, message, timestamp, stack }) => {
 });
 
 const logsDir = 'logs';
+const defaultConfig = {
+  maxsize: 5242880, // 5MB
+  maxFiles: 5,
+};
 
-const logger = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  format: combine(
-    timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-    errors({ stack: true }),
-    logFormat
-  ),
-  transports: [
+const createLogger = () => {
+  const transports = [
     new winston.transports.Console({
       format: combine(colorize(), logFormat),
     }),
+  ];
 
-    new winston.transports.File({
-      filename: path.join(logsDir, 'error.log'),
-      level: 'error',
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    }),
+  if (process.env.ENABLE_FILE_LOGGING === 'true') {
+    transports.push(
+      new winston.transports.File({
+        filename: path.join(logsDir, 'error.log'),
+        level: 'error',
+        ...defaultConfig,
+      }),
+      new winston.transports.File({
+        filename: path.join(logsDir, 'combined.log'),
+        ...defaultConfig,
+      })
+    );
+  }
 
-    new winston.transports.File({
-      filename: path.join(logsDir, 'combined.log'),
-      maxsize: 5242880, // 5MB
-      maxFiles: 5,
-    }),
-  ],
-  exitOnError: false,
-});
+  const logger = winston.createLogger({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format: combine(
+      timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+      errors({ stack: true }),
+      logFormat
+    ),
+    transports,
+    exitOnError: false,
+  });
 
-logger.stream = {
-  write: (message) => logger.info(message.trim()),
+  logger.stream = {
+    write: (message) => logger.info(message.trim()),
+  };
+
+  return logger;
 };
 
-export default logger;
+export default createLogger();
